7aa920d61cd175a1da0b3842ccfd39031cb51efa
tdoval <thiagodovalaraujo@gmail.com>
2024-08-20 20:37:51 -0300

    chore: Add navbar, sidebar, toolbar, and footer components to the editor page

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -3,6 +3,10 @@
 import { useEffect, useRef } from "react";
 
 import { useEditor } from "@/features/editor/hooks/use-editor";
+import { Navbar } from "@/features/editor/components/navbar";
+import { Sidebar } from "@/features/editor/components/sidebar";
+import { Toolbar } from "@/features/editor/components/toolbar";
+import { Footer } from "@/features/editor/components/footer";
 import { fabric } from "fabric";
 
 const Editor = () => {
@@ -24,8 +28,16 @@ const Editor = () => {
 
   return (
     <div className="h-full flex flex-col">
-      <div className="flex-1 h-full bg-muted" ref={containerRef}>
-        <canvas ref={canvasRef} />
+      <Navbar />
+      <div className="absolute h-[calc(100%-68px)] w-full top-[68px] flex">
+        <Sidebar />
+        <main className="bg-muted flex-1 ovewflow-auto relative flex flex-col">
+          <Toolbar />
+          <div className="flex-1 h-[calc(100%-124px)] bg-muted" ref={containerRef}>
+            <canvas ref={canvasRef} />
+          </div>
+          <Footer />
+        </main>
       </div>
     </div>
   );
