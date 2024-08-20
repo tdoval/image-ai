a9e68fffdc5dbd1dafeb3aeb307ed39a42a6948d
tdoval <thiagodovalaraujo@gmail.com>
2024-08-20 19:58:55 -0300

    feat: use-editor hook

---
+++ src/features/editor/components/editor.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+
+import { useEditor } from "@/features/editor/hooks/use-editor";
+import { fabric } from "fabric";
+
+const Editor = () => {
+  const { init } = useEditor();
+
+  const canvasRef = useRef(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const canvas = new fabric.Canvas(canvasRef.current, {
+      controlsAboveOverlay: true,
+      preserveObjectStacking: true,
+    });
+    init({
+      initialCanvas: canvas,
+      initialContainer: containerRef.current!,
+    });
+  }, [init]);
+
+  return (
+    <div className="h-full flex flex-col">
+      <div className="flex-1 h-full bg-muted" ref={containerRef}>
+        <canvas ref={canvasRef} />
+      </div>
+    </div>
+  );
+};
+
+export default Editor;
