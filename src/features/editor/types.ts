4f0f6753acdaba3116d434f0032cb23b24491fce
tdoval <thiagodovalaraujo@gmail.com>
2024-09-28 13:52:20 -0300

    feat: sendBack and BringFront

--- src/features/editor/types.ts
+++ src/features/editor/types.ts
@@ -115,6 +115,8 @@ export type BuildEditorProps = {
 };
 
 export interface Editor {
+  bringForward: () => void;
+  sendBackwards: () => void;
   changeStrokeColor: (value: string) => void;
   changeStrokeDashArray: (value: number[]) => void;
   changeStrokeWidth: (value: number) => void;
